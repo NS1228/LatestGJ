cc86ef828f08c9a3a638c6067d28dfaaa69712cc
Unknown <ns1228yt@gmail.com>
2019-11-09 19:52:37 +0000

    Ball Spawning + Shooting

---
+++ Assets/Scripts/Nil/Ball_Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Movement : MonoBehaviour
+{
+    public float timer;
+
+    public float speed = 3.0f;
+    public float xPos;
+    public float yPos;
+    public float zPos;
+    public Vector3 desiredPos;
+    void Start()
+    {
+        
+        xPos = Random.Range(-8.5f, 8.5f);
+        yPos = Random.Range(-4.5f, 5.5f);
+        zPos = Random.Range(-7f, -7f);
+
+        desiredPos = new Vector3(xPos, yPos, zPos);
+        timer = Time.time + 1.5f;
+    }
+
+    void Update()
+    {
+        if (Time.time >= timer)
+        {
+            Shoot();
+        }
+        speed = Random.Range(3.0f, 4.0f);
+
+    }
+
+    public void Shoot()
+    {
+        transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * speed);
+        if (Vector3.Distance(transform.position, desiredPos) <= 0.01f)
+        {
+            xPos = Random.Range(-4.5f, 4.5f);
+            desiredPos = new Vector3(xPos, yPos, zPos);
+
+        }
+    }
+    }
+
---
+++ Assets/Scripts/Nil/Ball_Spawn.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Spawn : MonoBehaviour
+{ 
+    public float currentBalls;
+    public float spawnSpeed;
+
+    public float interval;
+
+    public void Update ()
+    {
+        SpawnBall();
+        if(currentBalls >= 0 && currentBalls < 10)
+        {
+            spawnSpeed = 0;
+        }
+
+        if(currentBalls >= 10 && currentBalls < 20)
+        {
+            spawnSpeed = 1;
+        }
+        if (currentBalls >= 20 && currentBalls < 30)
+        {
+            spawnSpeed = 2;
+        }
+        if (currentBalls >= 30 && currentBalls < 40)
+        {
+            spawnSpeed = 3;
+        }
+        if (currentBalls >= 40 && currentBalls < 50)
+        {
+            spawnSpeed = 4;
+        }
+        if (currentBalls >= 50 )
+        {
+            spawnSpeed = 5;
+        }
+    }
+    public GameObject ball;
+    void SpawnBall()
+    {
+        if (spawnSpeed == 0 && Time.time >= interval)
+        {
+            Vector3 position = new Vector3(Random.Range(-23, 23), -4.5f, Random.Range(13, 60));
+            Instantiate(ball, position, Quaternion.identity);
+            currentBalls += 1;
+            interval = Time.time + 3f;
+        }
+
+        if (spawnSpeed == 1 && Time.time >= interval)
+        {
+            Vector3 position = new Vector3(Random.Range(-23, 23), -4.5f, Random.Range(13, 60));
+            Instantiate(ball, position, Quaternion.identity);
+            currentBalls += 1;
+            interval = Time.time + 2.5f;
+        }
+        if (spawnSpeed == 2 && Time.time >= interval)
+        {
+            Vector3 position = new Vector3(Random.Range(-23, 23), -4.5f, Random.Range(13, 60));
+            Instantiate(ball, position, Quaternion.identity);
+            currentBalls += 1;
+            interval = Time.time + 2f;
+        }
+        if (spawnSpeed == 3 && Time.time >= interval)
+        {
+            Vector3 position = new Vector3(Random.Range(-23, 23), -4.5f, Random.Range(13, 60));
+            Instantiate(ball, position, Quaternion.identity);
+            currentBalls += 1;
+            interval = Time.time + 1.5f;
+        }
+        if (spawnSpeed == 4 && Time.time >= interval)
+        {
+            Vector3 position = new Vector3(Random.Range(-23, 23), -4.5f, Random.Range(13, 60));
+            Instantiate(ball, position, Quaternion.identity);
+            currentBalls += 1;
+            interval = Time.time + 1f;
+        }
+        if (spawnSpeed == 5 && Time.time >= interval)
+        {
+            Vector3 position = new Vector3(Random.Range(-23, 23), -4.5f, Random.Range(13, 60));
+            Instantiate(ball, position, Quaternion.identity);
+            currentBalls += 1;
+            interval = Time.time + 0.5f;
+        }
+    }
+}
